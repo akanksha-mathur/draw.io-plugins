83fae7e06b0ca30559fbce5e2e0d8705a0255f26
akanksha-mathur <54259914+akanksha-mathur@users.noreply.github.com>
2019-08-22 13:01:08 +0530

    Update insertElementsWithMenu.js

--- insertElementsWithMenu.js
+++ insertElementsWithMenu.js
@@ -27,7 +27,7 @@ node.setAttribute('label', 'MyLabel');
 node.setAttribute('attribute1', 'value1');
             var car = {type:"Fiat", model:"500", color:"white"};
 //             var parent=graph.insertVertex(graph.getDefaultParent(), null, node, 40, 40, 80, 30);
-          var newElement=new mxCell(node,
+          var newElement=new mxCell(car,
                     new mxGeometry(pos.x, pos.y, 80, 80),
                     "ellipse;whiteSpace=wrap;html=1;");
         
